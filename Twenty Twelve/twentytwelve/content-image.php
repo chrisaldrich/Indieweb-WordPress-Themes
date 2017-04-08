7b1bdcda6d154b62acc0b49b976f3ce69d467bce
Chris Aldrich <christopherjaldrich@gmail.com>
2017-04-07 20:16:18 -0700

    mf2
    e-content & itemprop

--- Twenty Twelve/twentytwelve/content-image.php
+++ Twenty Twelve/twentytwelve/content-image.php
@@ -9,7 +9,7 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="entry-content">
+		<div class="entry-content e-content" itemprop="description articleBody">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 		</div><!-- .entry-content -->
 
