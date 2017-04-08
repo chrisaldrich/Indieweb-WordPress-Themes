c7f95373882d2b18ecbb9bac5d92d666861a859a
Chris Aldrich <christopherjaldrich@gmail.com>
2017-04-07 22:04:33 -0700

    mf2

--- Twenty Twelve/twentytwelve/tag.php
+++ Twenty Twelve/twentytwelve/tag.php
@@ -18,7 +18,7 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Tag Archives: %s', 'twentytwelve' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
+				<h1 class="archive-title entry-title p-entry-title"><?php printf( __( 'Tag Archives: %s', 'twentytwelve' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
 
 			<?php if ( tag_description() ) : // Show an optional tag description ?>
 				<div class="archive-meta"><?php echo tag_description(); ?></div>
